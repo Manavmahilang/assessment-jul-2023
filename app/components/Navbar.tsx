dbbc1910c4c8e7475f414fe00d5a93f3802624ec
Manav Mahilang <68985296+Manavmahilang@users.noreply.github.com>
2023-07-29 23:35:11 +0530

    Update Navbar.tsx
    removed dark mode navbar as theme toggle is not implemented completely

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = async () => {
   const session = await getServerSession(authOptions);
 
   return (
-    <div className='fixed backdrop-blur-sm bg-white/75 dark:bg-slate-900/75 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-center'>
+    <div className='fixed backdrop-blur-sm bg-white/75  z-50 top-0 left-0 right-0 h-20 border-b border-slate-300  shadow-sm flex items-center justify-center'>
       <div className='container max-w-7xl mx-auto w-full flex justify-between items-center'>
 
         <Link href='/' className='display inline-ce group'>
